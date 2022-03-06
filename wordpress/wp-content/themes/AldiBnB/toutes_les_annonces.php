03650d2a892d9ad7de2c6129069caabeafb0f571
ValentinR01 <vale.vr@icloud.com>
2022-03-06 10:08:35 +0100

    Add page renderer

--- wordpress/wp-content/themes/AldiBnB/toutes_les_annonces.php
+++ wordpress/wp-content/themes/AldiBnB/toutes_les_annonces.php
@@ -4,14 +4,10 @@ Template Name: Toutes les annonces
 */
 ?>
 <?php get_header(); ?>
-<?php
-    $query = new WP_Query(array(
-    'post_status' => 'publish'
-    ));
+<?php $query = new WP_Query(array('post_status' => 'publish')); ?>
 
 
-    while ($query->have_posts()) {
-    $query->the_post();
+    <?php while ($query->have_posts()) { $query->the_post();
     echo '<h5 class="card-title">' . get_the_title() . '</h5>';
     echo '<a href="' . get_the_permalink() . '">Lien annonce </a>';
     echo "<br>";
