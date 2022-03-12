8b8be9ec4569c3d8fa65b3e1db9f5a0f4c22cd59
lestox <78364535+lestox@users.noreply.github.com>
2022-03-12 13:26:12 +0100

    fix single-page

--- wordpress/wp-content/themes/AldiBnB/single-post.php
+++ wordpress/wp-content/themes/AldiBnB/single-post.php
@@ -2,6 +2,17 @@
 
 <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
+
+        <div>
+            <img src="<?php the_post_thumbnail_url(); ?>" alt="Image">
+            <div>
+                <h5><?php the_title(); ?></h5>
+                <p><?php the_content()?></p>
+                <p><small>Ecrit le : <?php the_date(); ?></small></p>
+
+            </div>
+        </div>
+
     <?php endwhile; ?>
 <?php else : ?>
     <h2>Pas de posts</h2>
