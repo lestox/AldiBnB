39ea3e502d7616b739ee7505ba82e1661d2041bd
Nassim YAZI <78474187+Nassim-dev@users.noreply.github.com>
2022-03-13 01:23:49 +0100

    style v.1

--- wordpress/wp-content/themes/AldiBnB/header.php
+++ wordpress/wp-content/themes/AldiBnB/header.php
@@ -1,25 +1,31 @@
 <!doctype html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
+    <title>AldiBnB</title>
     <?php wp_head(); ?>
 </head>
 
-<body>
-    <?php wp_nav_menu([
-        'theme_location' => 'header',
-        'container' => false,
-        'menu_class' => "navbar-nav me-auto mb-2 mb-lg-0"
-    ]); ?>
+<header>
+    <div id="flex-top-header">
+        <a id="logo" href="/">AldiBnb</a>
+        <?php wp_nav_menu([
+            'theme_location' => 'header',
+            'container' => false,
+            'menu_class' => "navbar-nav me-auto mb-2 mb-lg-0"
+        ]); ?>
 
-        <?php if ( is_user_logged_in() ) { ?>
-        <button type="button" onclick="window.location.href = '<?php echo wp_logout_url(get_permalink()); ?>';">Déconnexion
-            <?php } else { ?>
-            <button type="button" onclick="window.location.href = '<?php echo wp_login_url(get_permalink()); ?>';">Connexion
-                <?php } ?>
-            </button>
+            <?php
+            if ( is_user_logged_in() ) { ?>
+                <button class="button-log disconnect" type="button" onclick="window.location.href = '<?php echo wp_logout_url(get_permalink()); ?>';">Déconnexion</button>
+                <?php }
+            else { ?>
+                <button class="button-log connect" type="button" onclick="window.location.href = '<?php echo wp_login_url(get_permalink()); ?>';">Connexion</button>
+            <?php } ?>
+    </div>
     <?php get_search_form(); ?>
+</header>
