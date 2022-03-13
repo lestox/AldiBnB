eac98c303ace7142495c8e725033f171456679e3
Nassim YAZI <78474187+Nassim-dev@users.noreply.github.com>
2022-03-13 20:49:53 +0100

    styleV4

--- wordpress/wp-content/themes/AldiBnB/front-page.php
+++ wordpress/wp-content/themes/AldiBnB/front-page.php
@@ -2,21 +2,22 @@
 
     <section id="presentation">
         <div class="image_type">
-            <img src="https://source.unsplash.com/random/1920x1080/?house,villa"  alt="house pic"/>
+            <img src="https://source.unsplash.com/random/1920x1080/?villa"  alt="house pic"/>
         </div>
         <div class="text">
-            <h2>Un voyage, déplacement ou excursion ? AldiBnB est fait pour vous !</h2>
+            <h2>Envie de voyager et découvrir des habitations somptueuses ? <br>
+                <b>AldiBnB</b> est là pour vous !</h2>
             <div>
                 <i class="fa-solid fa-users fa-3x"></i>
                 <p> • <b>+5 millions</b> d’utilisateurs <br/>
                     • Plateforme de location <b>n°1*</b> mondial depuis 2022<br />
-                    <em>*source : hotels.com</em>
+                    <em>*source : nous-même.com</em>
                 </p>
             </div>
             <div>
                 <i class="fa-solid fa-location-dot fa-3x"></i>
                 <p>
-                    Nos locations sont disponibles dans toutes vos destinations
+                    Nos propositions de locations sont disponibles dans toutes vos destinations
                     <b>préférées</b>
                 </p>
             </div>
